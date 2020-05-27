e9f05dc4b5589da0ad744b806c305ae1c9077686
Bhoomik Bhamawat <bhoomikbhamawat.eee18@itbhu.ac.in>
2020-05-27 11:26:13 +0530

    Added animations

--- app/src/main/java/com/example/awareness/ui/MainActivity.java
+++ app/src/main/java/com/example/awareness/ui/MainActivity.java
@@ -8,6 +8,10 @@ import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
 import com.example.awareness.Constants;
@@ -17,29 +21,64 @@ public class MainActivity extends AppCompatActivity {
 
     VideoView videoView;
     SharedPreferences sharedPreferences;
-
+    Animation fromright,fromleft,fromdown;
+    ImageView jumio,shrushti,girl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        jumio = findViewById(R.id.jumio_logo);
+        shrushti = findViewById(R.id.imageView2);
+        girl = findViewById(R.id.imageView);
+        fromdown = AnimationUtils.loadAnimation(this,R.anim.fromdown);
+        fromleft = AnimationUtils.loadAnimation(this,R.anim.fromleft);
+        fromright = AnimationUtils.loadAnimation(this,R.anim.fromright);
         sharedPreferences = getSharedPreferences(Constants.MY_PREFERENCE, Context.MODE_PRIVATE);
         final String name = sharedPreferences.getString(Constants.name,"");
 
-        if(name.equals("")) {
 
-            Intent i = new Intent(MainActivity.this, Dashboard.class);
+
+
+
+
+        Animation.AnimationListener listener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if(name.equals("")) {
+
+                    Intent i = new Intent(MainActivity.this, Dashboard.class);
 //                    Intent i = new Intent(MainActivity.this, TestActivity.class);
 
-            startActivity(i);
-            finish();
-        }
-        else {
-            Constants.name_all=name;
-            startActivity(new Intent(MainActivity.this, Dashboard.class));
-            finish();
-        }
+                    startActivity(i);
+                    finish();
+                }
+                else {
+                    Constants.name_all=name;
+                    startActivity(new Intent(MainActivity.this, Dashboard.class));
+                    finish();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
+        fromdown.setAnimationListener(listener);
+        jumio.setAnimation(fromleft);
+        shrushti.setAnimation(fromright);
+        girl.startAnimation(fromdown);
+
+
 
 //        videoView = (VideoView) findViewById(R.id.videoView);
 //
