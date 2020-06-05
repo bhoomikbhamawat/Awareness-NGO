d1acd7ccdb1ce161dca60c5509fae95e3b219fb2
Bhoomik Bhamawat <bhoomikbhamawat.eee18@itbhu.ac.in>
2020-06-05 17:23:23 +0530

    Cretaorus and many other

---
+++ app/src/main/java/com/example/awareness/ui/AboutAdapter.java
@@ -0,0 +1,30 @@
+package com.example.awareness.ui;
+
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter;
+
+public class AboutAdapter extends SectionedRecyclerViewAdapter {
+
+    public AboutAdapter(List<AboutCategory> categories) {
+        super();
+        for (AboutCategory category : categories) {
+            addSection(category);
+        }
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder viewHolder = super.onCreateViewHolder(parent, viewType);
+        if (viewHolder instanceof AboutCategory.IndividualViewHolder) {
+            ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) viewHolder.itemView.getLayoutParams();
+            layoutParams.width = (parent.getWidth() / 2) - layoutParams.leftMargin - layoutParams.rightMargin;
+            viewHolder.itemView.setLayoutParams(layoutParams);
+        }
+        return viewHolder;
+    }
+}
--- app/src/main/java/com/example/awareness/ui/learningactivity/CreatorUs.java
+++ app/src/main/java/com/example/awareness/ui/learningactivity/CreatorUs.java
@@ -1,16 +1,58 @@
 package com.example.awareness.ui.learningactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.example.awareness.R;
+import com.example.awareness.ui.AboutAdapter;
+import com.example.awareness.ui.AboutCategory;
+import com.example.awareness.ui.AboutIndividual;
+import com.google.android.flexbox.FlexDirection;
+import com.google.android.flexbox.FlexboxLayoutManager;
+import com.google.android.flexbox.JustifyContent;
+
+import java.util.ArrayList;
 
 public class CreatorUs extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creator_us);
+
+
+
+        AboutAdapter aboutAdapter = new AboutAdapter(new ArrayList<AboutCategory>() {{
+            add(new AboutCategory("Core Developers", new ArrayList<AboutIndividual>() {{
+                add(new AboutIndividual("bhoomikbhamawat.eee18@iitbhu.ac.in", "Bhoomik Bhamawat", R.drawable.userdrawable,"8003044991"));
+                add(new AboutIndividual("monukumar.min18@iitbhu.ac.in", "Monu Kumar",  R.drawable.userdrawable,"8505089989"));
+                add(new AboutIndividual("djsweet94@gmail.com", "Divya Jain",  R.drawable.userdrawable,"9001847205"));
+                add(new AboutIndividual("aditijain8901@gmail.com", "Aditi Jain",  R.drawable.userdrawable,"7340666132"));
+            }}, CreatorUs.this));
+
+           /* add(new AboutCategory("Design", new ArrayList<AboutIndividual>() {{
+                add(new AboutIndividual("150040007", "Soham Khadtare", "soham.jpg"));
+            }}, context));*/
+
+
+
+            /*add(new AboutCategory("Alumni", new ArrayList<AboutIndividual>() {{
+                add(new AboutIndividual("abhijit.tomar", "Anant Gowadiya", "tomar.jpg"));
+                add(new AboutIndividual(null, "Abhinav Singh", "bijoy.jpg"));
+                 }}, context));*/
+
+        }});
+
+        RecyclerView aboutRecyclerView = findViewById(R.id.about_recycler_view);
+        aboutRecyclerView.setAdapter(aboutAdapter);
+
+        FlexboxLayoutManager manager = new FlexboxLayoutManager(CreatorUs.this, FlexDirection.ROW);
+        manager.setJustifyContent(JustifyContent.SPACE_EVENLY);
+        aboutRecyclerView.setLayoutManager(manager);
     }
 }
